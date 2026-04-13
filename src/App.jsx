9dd40910143bc207e26be0d134946d240afd8f4c
Josh-DEV15 <risingsun15052005@gmail.com>
2026-04-13 10:14:25 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Privacy from './components/Privacy';
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+      <Privacy />
+    </div>
+  );
+}
+
+export default App;
