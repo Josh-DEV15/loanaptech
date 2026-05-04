4912df45b3c45f2822c683c42d4b59a197efb457
Josh-DEV15 <risingsun15052005@gmail.com>
2026-05-04 11:01:08 +0100

    Added URL

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true 
+  origin: ["http://localhost:5173", "https://loanaptech-ten.vercel.app/"],
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser()); 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -42,7 +42,7 @@ export default function SignUp() {
         setLoading(true);
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/register', {
+            const response = await fetch('https://loanaptech-ten.vercel.app/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
