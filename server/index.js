f1efdb01d4ae961813241c7ec5cca71f2be2280c
Josh-DEV15 <risingsun15052005@gmail.com>
2026-05-04 12:05:18 +0100

    Fixed render issue

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://loanaptech-ten.vercel.app/"],
+  origin: ["http://localhost:5173", "https://loanaptech-ten.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
@@ -27,6 +27,6 @@ app.get('/api/test', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
