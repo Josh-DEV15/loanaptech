c4f9eb0cf27850f27cee5ed771e777cd6db2d58a
Josh-DEV15 <risingsun15052005@gmail.com>
2026-05-06 11:39:05 +0100

    Change server.js

--- server/server.js
+++ server/server.js
@@ -1,36 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-
-const connectDB = require('./config/db');
+const express = require("express");
 
 const app = express();
 
-// Middleware
-app.use(cors({
-  origin: ["http://localhost:5173", "https://loanaptech-ten.vercel.app"],
-  credentials: true
-}));
-
-app.use(express.json());
-app.use(cookieParser());
-
-// Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/loans', require('./routes/loans'));
-
-// Test route
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'API is working!' });
+app.get("/", (req, res) => {
+  res.send("Server works");
 });
 
 const PORT = process.env.PORT || 5000;
 
-// Start server FIRST
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Running on port ${PORT}`);
 });
-
-// THEN connect DB
-connectDB();
