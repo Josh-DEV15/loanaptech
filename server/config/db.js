9e0b225a507f93967df67df8f548216325eb974e
Josh-DEV15 <risingsun15052005@gmail.com>
2026-05-06 11:29:40 +0100

    Change db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,17 @@
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
-const express = require('express');
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
+
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
-    process.exit(1);
+
+    // REMOVE process.exit(1)
   }
 };
 
