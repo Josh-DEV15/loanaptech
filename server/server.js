5149a78ed53d76398d2e16fa75196951c0d50380
Josh-DEV15 <risingsun15052005@gmail.com>
2026-05-06 12:00:21 +0100

    Finally

--- server/server.js
+++ server/server.js
@@ -1,13 +1,33 @@
-const express = require("express");
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server works");
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://loanaptech-ten.vercel.app"
+  ],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/loans', require('./routes/loans'));
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API is working!' });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+connectDB();
