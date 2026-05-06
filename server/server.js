c26d4856cf9505c2fbc09e46d3ecaae96594d1c4
Josh-DEV15 <risingsun15052005@gmail.com>
2026-05-06 12:14:33 +0100

    Finally, again

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
+const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+
 require('dotenv').config();
 
 const connectDB = require('./config/db');
 
 const app = express();
 
+// Middleware
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -17,17 +20,26 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/loans', require('./routes/loans'));
 
+// Test route
 app.get('/api/test', (req, res) => {
   res.json({ message: 'API is working!' });
 });
 
+// Home route
+app.get('/', (req, res) => {
+  res.send('Server works');
+});
+
 const PORT = process.env.PORT || 5000;
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+// Connect DB after server starts
 connectDB();
