320f1941461410236b1737309f4ed1c55edca44a
Josh-DEV15 <risingsun15052005@gmail.com>
2026-05-08 11:42:27 +0100

    forgotten

--- src/pages/LoanDetails.jsx
+++ src/pages/LoanDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import "./Loans.css";
+import "./LoanDetails.css";
 
 function LoanDetails() {
   const { id } = useParams();
